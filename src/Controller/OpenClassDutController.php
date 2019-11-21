5b5c6b8ce169f4e649be6040b930fa0fa3be6535
Mathieu Echarte <mathieu.echarte1234@gmail.com>
2019-11-21 11:57:10 +0100

    Modification stages-formation-entreprises

--- src/Controller/OpenClassDutController.php
+++ src/Controller/OpenClassDutController.php
@@ -7,24 +7,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OpenClassDutController extends AbstractController
 {
-   /**
-    * @Route("/accueil", name="open_class_dut")
-    */
+   #/**
+   # * @Route("/accueil", name="open_class_dut")
+   # */
     public function index()
     {
         return $this->render('open_class_dut/index.html.twig', [
             'controller_name' => 'ControleurOpenClassDut',
         ]);
     }
-    
-    /**
-    * @Route("/accueil", name="open_class_dut")
-    */
-    public function index()
+
+    public function index2()
     {
         return $this->render('open_class_dut/entreprises.html.twig', [
             'controller_name' => 'ControleurOpenClassDut',
         ]);
     }
 
+    public function index3()
+    {
+        return $this->render('open_class_dut/formation.html.twig', [
+            'controller_name' => 'ControleurOpenClassDut',
+        ]);
+    }
+
+    public function index4($id)
+    {
+        return $this->render('open_class_dut/stages.html.twig', [
+            'StageID' => $id,
+        ]);
+    }
 }
