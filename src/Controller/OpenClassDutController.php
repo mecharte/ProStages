66269211067cc4740908ba576fc8b9f502a4e548
Mathieu Echarte <mathieu.echarte1234@gmail.com>
2019-11-21 11:03:42 +0100

    Creation premiere page

--- src/Controller/OpenClassDutController.php
+++ src/Controller/OpenClassDutController.php
@@ -7,11 +7,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OpenClassDutController extends AbstractController
 {
-    
+   /**
+    * @Route("/accueil", name="open_class_dut")
+    */
     public function index()
     {
         return $this->render('open_class_dut/index.html.twig', [
             'controller_name' => 'ControleurOpenClassDut',
         ]);
     }
+    
+    /**
+    * @Route("/accueil", name="open_class_dut")
+    */
+    public function index()
+    {
+        return $this->render('open_class_dut/entreprises.html.twig', [
+            'controller_name' => 'ControleurOpenClassDut',
+        ]);
+    }
+
 }
