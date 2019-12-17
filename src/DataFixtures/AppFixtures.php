bf6cd4acf078a679f0bc2515e96e9000befeabb1
Mathieu Echarte <mathieu.echarte1234@gmail.com>
2019-12-17 18:42:16 +0100

    Création de donnée avec FAKER et en dure

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,11 +5,14 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Formation;
-
+use App\Entity\Entreprise;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        // Création d'un générateur de donnée avec FAKER
+        $faker = \Faker\Factory::create('fr_FR');
+        // Création de donnée en dure
         $Formation_DUT_Informatique = new Formation();
         $Formation_DUT_Informatique->setNomLong("Diplome Universitaire et Technologique en Informatique");
         $Formation_DUT_Informatique->setNomCourt("DUT_INFO");
@@ -25,6 +28,20 @@ class AppFixtures extends Fixture
         $Formation_DU_TIC->setNomCourt("DU TIC");
         $manager->persist($Formation_DU_TIC);
 
+        // Création de donnée avec FAKER
+        $nbEntreprises = 15;
+        for($i=1;$i <= $nbEntreprises;$i++){
+            $Entreprise_1= new Entreprise();
+            $Entreprise_1->setNom($faker->realText($maxNbChars = 200, $indexSize = 2));
+            $Entreprise_1->setActivite($faker->regexify('Bonjour[A-Z],[A-Z]{1,45}'));
+            $Entreprise_1->setAdresse($faker->address);
+            $Entreprise_1->setSiteWeb($faker->url);
+            $manager->persist($Entreprise_1);
+        }
+        
+
+        //Envoyer les données en BD
         $manager->flush();
+
     }
 }
