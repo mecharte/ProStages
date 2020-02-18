97c754e2f9a54d10ed1c9744ce3f1f777c2bedcc
Mathieu Echarte <mathieu.echarte1234@gmail.com>
2020-02-18 13:29:47 +0100

    Finalisation des assert\regex création de la dernière assert sur la rue

--- src/Entity/Entreprise.php
+++ src/Entity/Entreprise.php
@@ -40,7 +40,8 @@ class Entreprise
     /**
      * @ORM\Column(type="string", length=150)
      * @Assert\Regex(pattern="#(rue|avenue|boulevard|impasse|allée|place|voie)#",message="Le type de route/voie semble incorrect.")
-     *  @Assert\Regex(pattern="#( [0-9]{5} )#",message="Il semble y avoir un problème avec le code postal.")
+     * @Assert\Regex(pattern="#( [0-9]{5} )#",message="Il semble y avoir un problème avec le code postal.")
+     * @Assert\Regex(pattern="#(^[1-9]([0-9])*?(bis| bis))#",message ="Le numéro de rue semble incorrect.")
      */
     private $adresse;
 
